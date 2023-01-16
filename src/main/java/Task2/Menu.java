2d16a1fe593e2ae6befb0cfd4d310f92d4a3df0d
AleksandraKasperowicz <aleksandrakasperowicz@wp.pl>
2023-01-16 16:48:05 +0100

    Add changes to pizza task

--- src/main/java/Task2/Menu.java
+++ src/main/java/Task2/Menu.java
@@ -11,34 +11,12 @@ public class Menu {
             menu.add(new Pizza("Salami", new Ingredient[] {Ingredient.MOZARELLA,Ingredient.CEBULA, Ingredient.SALAMI}, 100, false));
             menu.add(new Pizza("Vegetarian", new Ingredient[] {Ingredient.MOZARELLA,Ingredient.CEBULA, Ingredient.SELER, Ingredient.POMIDOR, Ingredient.PAPRYKA}, 450, true));
 
-            menu.stream()
-                    .filter(Pizza::isVegetarian)
-                    .forEach(System.out::println);
-
-            System.out.println();
-            menu.stream()
-                    .filter(pizza -> pizza.getIngredients().contains(Ingredient.SELER))
-                    .forEach(System.out::println);
-
-            System.out.println();
-            menu.stream()
-                    .filter(Pizza::isVegetarian)
-                    .filter(pizza -> pizza.getIngredients().contains(Ingredient.POMIDOR) || pizza.getIngredients().contains(Ingredient.PAPRYKA) )
-                    .forEach(System.out::println);
-
-            System.out.println();
-            System.out.println(menu.stream()
-                    .allMatch(pizza -> pizza.getIngredients().contains(Ingredient.MOZARELLA))
-                    ? "Wszystkie zawierają mozarellę" : "Nie wszystkie zawierają mozarellę");
-
-            System.out.println("Max calories:");
-            System.out.println(menu.stream()
-                    .max((pizza1, pizza2) -> Integer.compare(pizza1.getCalories(), pizza2.getCalories())).get());
-
-            System.out.println();
-            System.out.println("Min calories:");
-            System.out.println(menu.stream()
-                    .min((pizza1, pizza2) -> Integer.compare(pizza1.getCalories(), pizza2.getCalories())).get());
+        System.out.println(PizzaFilter.findVegetarian(menu));
+        System.out.println(PizzaFilter.findWithSeler(menu));
+        System.out.println(PizzaFilter.findWithPomidorAndPapryka(menu));
+        System.out.println(PizzaFilter.findWithMozarella(menu));
+        System.out.println(PizzaFilter.findMaxCall(menu));
+        System.out.println(PizzaFilter.findMinCall(menu));
 
         }
 }
